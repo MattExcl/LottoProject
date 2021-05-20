a710e66ea51b8c10c0f680eb26409900d33a761e
004goldenwolff <84527746+004goldenwolff@users.noreply.github.com>
2021-05-20 12:12:44 -0400

    Update Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -2,5 +2,24 @@ package loto;
 import java.util.Scanner;
 
 public class Lotto {
+/*
+ * to do:
+ * import numbers
+ * number genorator
+ * number checker
+ * total $
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 
+	public static void main(String[] args) {	
+
+  
+  }
+	
+  
+  
 }
