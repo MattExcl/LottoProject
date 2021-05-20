02764a1982f3b9f251a87fd0a712d8abb06ce2bf
004goldenwolff <84527746+004goldenwolff@users.noreply.github.com>
2021-05-20 12:36:11 -0400

    Update Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -20,6 +20,14 @@ public static void main(String[] args) {
 	int lottoNumbers = 0;
 	int totalNums = 0;
 	int input;
+	
+	System.out.println("Welcom to lotto, What are your 5 numbers? press return after each number:");
+		
+	for(int i =0;i<5;i++) {
+		input = in.nextInt();
+					}
+	
+	
 	input = in.nextInt();
 	
 	while(totalNums < 5){
