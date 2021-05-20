03d603ad9b83db0eaf2b03dc9114b3125d07a4bd
MattExcl <73499730+MattExcl@users.noreply.github.com>
2021-05-20 12:21:29 -0400

    Update Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -15,10 +15,17 @@ public class Lotto {
  * 
  * 
  */
-
-	public static void main(String[] args) {	
-
-  
+public static void main(String[] args) {	
+	Scanner in = new Scanner(System.in);
+	int lottoNumbers = 0;
+	int totalNums = 0;
+	int input;
+	input = in.Next(int);
+	
+	while(totalNums < 5){
+		int lottoNumbers = input; //Here is where the numbers will be that get added to the array
+		totalNums++;
+	}
   }
 	
   
