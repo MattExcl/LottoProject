f2eb83270baf6cd41cc4d68ea9fed0a20c212f34
MattExcl <73499730+MattExcl@users.noreply.github.com>
2021-05-20 12:12:24 -0400

    Create Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -1,20 +1,6 @@
 package loto;
+import java.util.Scanner;
 
 public class Lotto {
-/*
- * to do:
- * import numbers
- * number genorator
- * number checker
- * total $
- * 
- * 
- * 
- * 
- * 
- */
 
-	public static void main(String[] args) {	
-	}
-	
 }
