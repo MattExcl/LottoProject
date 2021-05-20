095ff9531dae1f289709e0f8f86bf394d4a09a03
004goldenwolff <84527746+004goldenwolff@users.noreply.github.com>
2021-05-20 12:13:42 -0400

    Update Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -6,9 +6,10 @@ public class Lotto {
  * to do:
  * import numbers
  * number genorator
+ thing to check if there are dups in the nmber
  * number checker
  * total $
- * 
+ * frequency table
  * 
  * 
  * 
