f4fa6c680668a7daae8c05ed323d700eedb20eb9
004goldenwolff <84527746+004goldenwolff@users.noreply.github.com>
2021-05-20 12:27:15 -0400

    Update Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -20,7 +20,10 @@ public static void main(String[] args) {
 	int lottoNumbers = 0;
 	int totalNums = 0;
 	int input;
-	input = in.Next(int);
+	int [] given = new int[5]; 
+	int [] round = new int[5]; 
+		
+	input = in.Next(int);}
 	
 	while(totalNums < 5){
 		int lottoNumbers = input; //Here is where the numbers will be that get added to the array
