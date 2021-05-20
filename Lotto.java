162c07f01e0a38e3386c3e800ab8fa170ee77452
004goldenwolff <84527746+004goldenwolff@users.noreply.github.com>
2021-05-20 12:40:50 -0400

    Update Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -23,6 +23,10 @@ public static void main(String[] args) {
 	
 	System.out.println("Welcom to lotto, What are your 5 numbers? press return after each number:");
 		
+		for(int i =0;i<5;i++) {
+			System.out.print("\n"+(i+1) +": ");given[i] = in.nextInt();
+			}
+		System.out.println(Arrays.toString(given));
 	for(int i =0;i<5;i++) {
 		input = in.nextInt();
 					}
