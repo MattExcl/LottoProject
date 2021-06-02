a4a9247cfd8cd98b3e3f680b6f5e6dce5962768e
004goldenwolff <84527746+004goldenwolff@users.noreply.github.com>
2021-06-02 11:53:53 -0400

    Update Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -1,38 +1,46 @@
+
 package loto;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lotto {
 /*
  * to do:
- * import numbers
- * number genorator
+ * import numbers----// add loop that checks if it num has been put in yet/ if its a dupe
+ *
+ * 
+ * number genorator		 done
  
  * number checker
  * total $
  * frequency table
  * 
  * 
+ * //check if win int count num=0 if it = one of the numbers, then it adds one  big for loop that repeats 104 times
  * 
  * 
+ * 
+ * 
+ * winnings are held in an array,index refers to howmany matching.
+ * at end have it do award*3 in array
  */
-public static void main(String[] args) {	
-	Scanner in = new Scanner(System.in);
-		int numofjackpot = 0;
-		int numofmatching=-1;
+
+	public static void main(String[] args) {	
+Scanner in = new Scanner(System.in);
+		int numofmatching=0;
 		int input=-1;
 		int [] given = new int[5]; 
 		int [] round = new int[5]; 
 		int [] winnings = new int[5]; 
+		double[]fqcy = new double[21]; 
 		System.out.println("Welcom to lotto, What are your 5 numbers? press return after each number:");
 		
 		for(int i =0;i<5;i++) {  
 			System.out.print((i+1) +": ");   
-			input = in.nextInt();   
-		
-		System.out.println(Arrays.toString(given));
-	
-
-			if(input<20 && input>0) {
+			input =(int)((Math.random()*20)+1);//in.nextInt();   
+			if(input<21 && input>0) {
+				
+				
 				given[i]=input; 
 				}
 			else {
@@ -40,9 +48,7 @@ public static void main(String[] args) {
 				i--;
 				}  
 			}// put in a for loop that checks once all are inputed, to see if any repeat could use method
-		
-	
-	for(int i =0;i<5;i++) {//this is me trying to do a checker to check for dupes
+		/*for(int i =0;i<5;i++) {
 			for(int j =0;j<5;j++) {
 				if((given[i]==given[j])&&(i!=j)) {
 					System.out.print(errorForInput()+"\n");
@@ -51,7 +57,7 @@ public static void main(String[] args) {
 					}
 				}
 			}
-		
+		*/
 		
 		System.out.println("Your Ticket: "+Arrays.toString(given));
 		
@@ -71,19 +77,40 @@ public static void main(String[] args) {
 //}
 	  
 		
- // Random num gen
+  // Random num gen
 		//Array given[5],   int [] given = new int[5]; 
 		for(int i =0;i<5;i++) {
 			round[i]=(int)((Math.random()*20)+1);		// might not need this line
 			for(int j =0;j<5;j++) {
-				if((round[i]==round[j])&&(i!=j)) {
-						round[j]=(int)((Math.random()*20)+1);j--;
-					 
-					}
+				if((i!=j)&&(round[i]==round[j])) {
+						round[j]=(int)((Math.random()*20)+1);j=0; 
+					}		
 				}
 			}
+		
+		for(int i =0;i<5;i++) {
+			fqcy[round[i]]++;
+				}
+		
+		
 		System.out.println("winning numbers: "+Arrays.toString(round));
+		
+		
+		//for(int i =0;i<5;i++) { for(int j =0;j<5;j++) {
+				//if((round[i]==round[j])&&(i!=j)) {
 		//checker
+		for(int t =0;t<5;t++)
+		numofmatching=-1;
+		for(int i =0;i<5;i++) {
+			
+		
+			for(int j =0;j<5;j++) {
+				if(given[i]==round[j]) {
+					numofmatching++; }
+				}
+				
+				
+				
 // random grab num from an array list
 	/*
 	 * 	 if(w.charAt(i)== 'a'||w.charAt(i)== 'e'||w.charAt(i)== 'i'||w.charAt(i)== 'o'||w.charAt(i)== 'u') {// maybe use this?
@@ -97,20 +124,18 @@ public static void main(String[] args) {
 	//	System.out.print((i+1) +": "); input = in.nextInt();
 	//	if(input<20 && input>0) {
 	//		given[i]=input; }
-
 	
+	
+		//winnings[numofmatching]++; 
+		System.out.print("--- Thanks for Playing!---\n\n Below is the Frequency table and winnings\n\nFrequency Table: \n");	
 		
-		winnings[numofmatching]++; 	
+			for(int i =1;i<=20;i++) { if(i%10==1) {System.out.println();} System.out.print((i)+": "+(int)(((fqcy[i]/104.0)*10))+"\t");}
+					
 	}
-
-
-
-
+	
+	
+	
 public static String errorForInput() {return" ---Please input a number between 1 and 20 inclusive, that is also not one that you have already inputed: --- ";}
-
-
-
-
 }
 
 
